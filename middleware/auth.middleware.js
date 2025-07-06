9cf6eb7c014e8f7d731a712e2d0000b17a936236
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2025-07-06 10:44:28 +0545

    Initialize Express router and import User model and JWT for authentication

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken")
 module.exports = async function(req,res,next) {
-    const token = req.header['authorization']
+    const token = req.header['authorization'];
     if (!token)
         return res.status(403).send("Token Missing");
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        req.userid = decoded.id
-        next()
+        req.userid = decoded.id;
+        next();
     }
     catch(err) {
         res.status(401).send("Unauthorized");
