03e174586c8a3d3ebe2161f2dd15ce331a6ee1da
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2025-07-06 11:04:35 +0545

    Add protected GET route to fetch all students using auth middleware

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -2,3 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { Student } = require('../models');
 const authMiddleware = require('../middleware/auth.middleware');
+
+router.get('/', authMiddleware, async (req, res) =>{
+    const students = await Student.findAll();
+    res.json(students);
+});
