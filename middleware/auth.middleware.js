edeaaaa833c22257e907e73e03e23af2b3190544
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2025-07-06 10:19:12 +0545

    handling missing token

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,10 @@
 const jwt = require("jsonwebtoken")
 module.exports = async function(req,res,next) {
     const token = req.header['authorization']
+    if (!token)
+        return res.status(403).send("Token Missing");
+
+    
+
     
 }
