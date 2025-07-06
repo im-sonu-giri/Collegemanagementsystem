80a93260a5d42ae4300c1ad0464fbb080150f70b
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2025-07-06 09:45:48 +0545

    student name email and course datatype string  table return

--- models/student.model.js
+++ models/student.model.js
@@ -2,5 +2,9 @@ const { DataTypes } = require("sequelize");
 
 
 module.exports = (sequelize, DataTypes) => {
-    
+    return sequelize.define {
+        name= DataTypes.STRING,
+        email =DataTypes.STRING,
+        course = DataTypes.STRING,
+    }
 }
