a230dd54c9b05628017a33409119dfaca6f7a417
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2025-07-06 17:44:36 +0545

    split for Bearer token

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,8 +1,12 @@
 const jwt = require("jsonwebtoken")
 module.exports = function(req, res, next) {
-    const token = req.headers['authorization'];
-    if (!token)
+    const authHeader= req.headers['authorization'];
+    if (!authHeader)
         return res.status(401).send("Token Missing");
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).send("token Missing")
+    }
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.userid = decoded.id;
