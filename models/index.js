7405c8a2917924fa193833f03b257acb7859ab9a
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2025-07-06 09:14:52 +0545

    using module.export for all instances

--- models/index.js
+++ models/index.js
@@ -11,4 +11,4 @@ const sequelize = new sequelize(
 const user = require("./user.model")(sequelize, DataTypes)
 const student = require("./student.model")(sequelize, DataTypes)
 
-return{sequelize, user, student}
+module.exports = {sequelize, user, student}
