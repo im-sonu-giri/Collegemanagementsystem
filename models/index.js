32e305f694da66ce7df43a133110086853d239e6
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2025-07-06 08:59:28 +0545

    adding host and database type

--- models/index.js
+++ models/index.js
@@ -2,5 +2,8 @@ const { sequelize , DataTypes } = require('sequelize')
 const sequelize = new sequelize(
     process.env.DB_USER,
     process.env.DB_NAME,
-    process.env.DB_PASSWORD
+    process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: 'postgres'
+    }
     )
